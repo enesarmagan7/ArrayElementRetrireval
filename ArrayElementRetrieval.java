6665ed39c6f32df3fbca3690d5bd7a2e781f5536
enesarmagan7 <144248210+enesarmagan7@users.noreply.github.com>
2023-10-18 15:30:13 +0300

    Update ArrayElementRetrieval.java

--- ArrayElementRetrieval.java
+++ ArrayElementRetrieval.java
@@ -20,7 +20,7 @@ public class ArrayElementRetrieval {
         }
     }
 
-    // Method to retrieve the element at the specified index
+     //  alınan indeksteki elemanı döndüren  metot
     public static int retrieveElement(int[] array, int index) {
         if (index < 0 || index >= array.length) {
             throw new ArrayIndexOutOfBoundsException();
